2378ea9a702c533664723cf02c52c7db139f36a9
Alex Buckley <abuckley@nrnb-head.ucsd.edu>
2017-06-12 11:43:21 -0700

    added script to parse samtools mpileup output

--- parse.mpileup.py
+++ parse.mpileup.py
@@ -12,6 +12,23 @@ def parse_info(x):
         info_dict[tag]=val
     return info_dict['AD']  
 
+'''
+if ref and alt end in the same sequence, trim off base pairs from the end one at a time to get smallest indel notation
+always leave at least one ref/alt base per GATK VCF notation
+ex:     AGATGAT AGAT
+becomes AGAT    A
+'''
+
+def simplify_indel(ref,alt):
+    if ref[-1]== alt[-1] and len(ref) > 1 and len(alt) >1:
+        while ref[-1] == alt[-1]  and len(ref) > 1 and len(alt) >1:
+            ref=ref[0:-1]
+            alt=alt[0:-1]
+        return(ref,alt)
+# if indel can't be shortened, output as-is
+    else:
+        return(ref,alt)
+
 import fileinput
 
 #print header line
@@ -37,7 +54,7 @@ for line in fileinput.input():
     if alt == "<*>":
         continue
 #for multiallelic sites, output each allele on a separate line
-    elif "," in alt:
+    if "," in alt:
         alt_list=alt.split(',')
         for i in range(len(alt_list)):
 #skip sites with no variation
@@ -45,10 +62,15 @@ for line in fileinput.input():
             alt_depthi=alt_depth[i]
             if alti == "<*>":
                 continue
-            else:
-                format= "%s\t%s\t%s\t%s\t%s\t%s" % (chr,pos,ref,alti,ref_depth,alt_depthi )
-                print format
+	    elif len(ref) > 1 or len(alt) > 1:
+		ref,alti=simplify_indel(ref,alti) 
+            format= "%s\t%s\t%s\t%s\t%s\t%s" % (chr,pos,ref,alti,ref_depth,alt_depthi )
+            print format
         
+    elif len(ref) > 1 or len(alt) > 1:
+        ref,alt=simplify_indel(ref,alt)
+	format= "%s\t%s\t%s\t%s\t%s\t%s" % (chr,pos,ref,alt,ref_depth,alt_depth[0] )
+        print format
     else:
-        format= "%s\t%s\t%s\t%s\t%s\t%s" % (chr,pos,ref,alt,ref_depth,alt_depth[0] )
+	format= "%s\t%s\t%s\t%s\t%s\t%s" % (chr,pos,ref,alt,ref_depth,alt_depth[0] )
         print format
